09475c165e62074d92265c6113158e53518f9dc6
lulei <lulei@didichuxing.com>
2018-07-04 15:15:44 +0800

    basic usage

---
+++ app/src/main/java/com/ll/components/components/MyObserver.java
@@ -0,0 +1,43 @@
+package com.ll.components.components;
+
+import android.arch.lifecycle.Lifecycle;
+import android.arch.lifecycle.LifecycleObserver;
+import android.arch.lifecycle.LifecycleOwner;
+import android.arch.lifecycle.OnLifecycleEvent;
+import android.util.Log;
+
+public class MyObserver implements LifecycleObserver {
+
+    @OnLifecycleEvent(Lifecycle.Event.ON_CREATE)
+    public void onCreate(LifecycleOwner owner) {
+        Log.e("TAG-----onCreate ", " onCreate " + owner.getClass().getSimpleName());
+    }
+
+    @OnLifecycleEvent(Lifecycle.Event.ON_START)
+    public void onStart(LifecycleOwner owner) {
+        Log.e("TAG-----onStart ", " onStart " + owner.getClass().getSimpleName());
+    }
+
+    @OnLifecycleEvent(Lifecycle.Event.ON_RESUME)
+    public void onResume(LifecycleOwner owner) {
+        Log.e("TAG-----onResume ", " onResume " + owner.getClass().getSimpleName());
+    }
+
+    @OnLifecycleEvent(Lifecycle.Event.ON_PAUSE)
+    public void onPause(LifecycleOwner owner) {
+        Log.e("TAG-----onPause ", " onPause " + owner.getClass().getSimpleName());
+    }
+
+    @OnLifecycleEvent(Lifecycle.Event.ON_STOP)
+    public void onStop(LifecycleOwner owner) {
+        Log.e("TAG-----onStop ", " onStop " + owner.getClass().getSimpleName());
+    }
+
+
+    @OnLifecycleEvent(Lifecycle.Event.ON_DESTROY)
+    public void onDestroy(LifecycleOwner owner) {
+        Log.e("TAG-----onDestroy ", " onDestroy " + owner.getClass().getSimpleName());
+    }
+
+
+}
